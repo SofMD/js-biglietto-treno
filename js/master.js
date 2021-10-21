7f72e858d3416c33a52655439923ae7b4e67687c
sofMD <xsofiamaria.32@gmail.com>
2021-10-21 16:31:32 +0200

    senza console.log

--- js/master.js
+++ js/master.js
@@ -1,27 +1,22 @@
 // chiedere il numero di chilometri 
 const chilometri = parseInt( prompt('Inserisci il numero di kilometri'));
-console.log(chilometri);
 
 // chiedere età
 const etàUtente = parseInt( prompt('Inserisci la tua età'));
-console.log(etàUtente);
 
 // calcola il prezzo del biglietto che è definito in base ai km (0.21 € al km)
 let prezzoBiglietto = chilometri * 0.21;
-console.log(prezzoBiglietto);
 
 // calcolo prezzo per minorenni (sconto 20%)
 let prezzoScontato;
 
 if (etàUtente < 18) {
     prezzoScontato = (prezzoBiglietto * 20) / 100;
-    console.log(prezzoScontato);
 }
 
 // calcolo prezzo per gli over 65 (sconto 40%)
 if (etàUtente >= 65) {
     prezzoScontato = (prezzoBiglietto * 40) / 100;
-    console.log(prezzoScontato);
 }
 
 // prezzo Finale
